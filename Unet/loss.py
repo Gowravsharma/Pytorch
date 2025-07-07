23bafd972487164e8d589ee2d62d2f4ceea7fddc
Gowravsharma <gowravsharma8849@gmail.com>
2025-07-07 11:32:01 +0530

    completed the Binary focal Loss function

--- Unet/loss.py
+++ Unet/loss.py
@@ -1,20 +1,38 @@
 import torch
 from torch import nn
+from collections import Counter
 
 class BinaryFocalLoss(nn.Module):
-  def __init__(self, alpha = 0.5, gamma = 2, reduction = 'mean'):
+  def __init__(self, weights = None, gamma = 2, reduction = 'mean', sigmoid = True):
     super().__init__()
-    self.alpha = alpha
+    self.weights = weights
     self.gamma = gamma
     self.reduction = reduction
+    self.sigmoid = sigmoid
+    self.count = 0
   
   def forward(self, y_pred, y_true):
-    probs = torch.sigmoid(y_pred)
+    if y_pred.shape != y_true.shape:
+      y_pred = y_pred.squeeze(1)
+    y_true = y_true.float()
+
+    if self.weights is None and self.count == 0:
+      flat_true = y_true.cpu().detach().numpy().astype(int).flatten()
+      num_samples = y_true.numel()
+      counts = Counter(flat_true)
+      w0 = num_samples/(counts.get(0, 1e-6)*2)
+      w1 = num_samples/(counts.get(1, 1e-6)*2)
+
+      self.weights = torch.tensor([w0,w1], dtype = torch.float32, device = y_true.device)
+      self.count += 1
+
+    probs = torch.sigmoid(y_pred) if self.sigmoid is True else y_pred
     probs = probs.clamp(min = 1e-6, max = 1 - 1e-6)
     
-    pt = probs * y_true + (1- probs) *(1-y_true)
+    pt = probs * y_true + (1-probs) *(1-y_true)
     focal_term = (1-pt)**self.gamma
-    loss = - self.alpha*focal_term*torch.log(pt)
+
+    loss = - (self.weights[0]*(1-y_true) + self.weights[1]*(y_true))*focal_term*torch.log(pt)
     
     if self.reduction == 'mean':
       return loss.mean()
@@ -22,3 +40,10 @@ class BinaryFocalLoss(nn.Module):
       return loss.sum()
     else:
       return loss
+
+if __name__ == '__main__':
+  loss_fn = BinaryFocalLoss()
+y_pred = torch.randn(8, 1, 128, 128)  # logits
+y_true = torch.randint(0, 2, (8, 128, 128))  # binary labels
+loss = loss_fn(y_pred, y_true)
+print(loss)
